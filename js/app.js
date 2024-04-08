7fa75fbe5dcc22500ce70ecf6aad0c55ddea76b5
Katlego Kgotse <katlegokgotse88@gmail.com>
2024-04-08 16:51:13 +0200

    Copy Function

--- js/app.js
+++ js/app.js
@@ -5,4 +5,10 @@ const txt = document.getElementById('u');
 
 function handleSubmit(){
   border.style.borderRadius = `${input.value}px`
+  txt.textContent = `border-radius: ${input.value}px`
+}
+
+function copyText(){
+    navigator.clipboard.writeText(txt.innerHTML)
+    alert("Text Copied")
 }
