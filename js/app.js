166d97542ce570181db440583883672fc57d6c76
Katlego Kgotse <katlegokgotse88@gmail.com>
2024-04-08 17:20:55 +0200

    UI fix

--- js/app.js
+++ js/app.js
@@ -1,13 +1,16 @@
+//Declarations and Initialisation
 const input = document.getElementById('edtBorder');
 const border = document.getElementById('borderRad');
-
+const copy = document.getElementById('btnCopy')
 const txt = document.getElementById('u');
 
+//Handles the user input
 function handleSubmit(){
   border.style.borderRadius = `${input.value}px`
   txt.textContent = `border-radius: ${input.value}px`
 }
 
+//Enables the user to copy the information
 function copyText(){
     navigator.clipboard.writeText(txt.innerHTML)
     alert("Text Copied")
